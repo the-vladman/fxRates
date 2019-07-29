d3921c761b64c26af8332d0c889024eefdb8b861
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-07-28 21:39:46 -0500

    add antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Layout, Typography } from 'antd';
+const { Header, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <p>FX Rates</p>
-    </div>
+    <Layout className="layout">
+    <Header>
+      <Typography>
+        <Title style={{color: 'white' }}>Fx Rates</Title>
+      </Typography>
+    </Header>
+    <Content style={{ height: 800, padding: '0 50px' }}>
+      
+    </Content>
+  </Layout>
   );
 }
 
