b8393799152df6a51b822abb1ac990dfe5ebe673
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-07-29 19:04:22 -0500

    (helper) dates

---
+++ src/helpers/dates.js
@@ -0,0 +1,23 @@
+const removeDays = (date, days) => {
+    var result = new Date(date);
+    result.setDate(result.getDate() - days);
+    return result;
+}
+const addZero = (number) => number < 10 ? `0${number}`: number;
+
+const getYear = (day) => day.getFullYear();
+
+const getMonth = (day) => addZero(day.getMonth() + 1);
+
+const getDay = (day) => addZero(day.getDate());
+
+const getDayLess = (day, days) => {
+    const dayLess = removeDays(day, days);
+    const dayLessString = `${getYear(dayLess)}-${getMonth(dayLess)}-${getDay(dayLess)}`;
+    console.log(dayLessString);
+    return dayLessString
+}
+
+  export default {
+    getDayLess
+  }
