d6400d398a3fff560bda77b59eee297c454ca7c0
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-07-30 18:13:03 -0500

    update getLatestStatic
    use Prettier

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Layout, Row, Col, Typography, message, Card } from 'antd';
-import fetchService from './fetchService';
-import { connect } from 'react-redux';
-import { rates } from './actions/rates';
-import { CHistoricalLineChart } from './containers/CHistoricalLineChart';
-import { CSelectCurrency } from './containers/CSelectCurrency';
+import React, { Component } from "react";
+import "./App.css";
+import { Layout, Row, Col, Typography, message, Card } from "antd";
+import fetchService from "./fetchService";
+import { connect } from "react-redux";
+import { rates } from "./actions/rates";
+import { CHistoricalLineChart } from "./containers/CHistoricalLineChart";
+import { CSelectCurrency } from "./containers/CSelectCurrency";
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
-
 class AppView extends Component {
-
-  componentDidMount(){
-    this.getLatest()
+  async componentDidMount() {
+    await this.getLatest();
   }
 
   async getLatest() {
+    const { fetchLatest, addHistorical } = this.props;
     try {
-      const latest = await fetchService.getLatestStatic();
-      this.props.fetchLatest(latest);
+      await fetchService.getLatestStatic(fetchLatest, addHistorical);
     } catch (error) {
-      message.error(error)
+      message.error(error);
     }
   }
 
   render() {
-    const { historicalRates } = this.props;
     return (
       <Layout className="layout">
         <Header>
           <Typography>
-            <Title style={{color: 'white' }}>Fx Rates</Title>
+            <Title style={{ color: "white" }}>Fx Rates</Title>
           </Typography>
         </Header>
-        <Content style={{ height: 800, padding: '0 50px' }}>
-
+        <Content style={{ height: 800, padding: "0 50px" }}>
           <Row>
             <Col span={20} offset={2}>
               <Card title="Default size card">
-                <CSelectCurrency/>
-                <CHistoricalLineChart/>
+                <CSelectCurrency />
+                <CHistoricalLineChart />
               </Card>
             </Col>
           </Row>
@@ -53,19 +49,20 @@ class AppView extends Component {
 const mapStateToProps = state => {
   return {
     historicalRates: state.historicalRates,
-    today: state.today,
-  }
-}
+    today: state.today
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-      fetchLatest: (latest) => dispatch(rates.fetchLatest(latest))
-  }
-}
+    fetchLatest: latest => dispatch(rates.fetchLatest(latest)),
+    addHistorical: historical => dispatch(rates.addHistorical(historical))
+  };
+};
 
 const App = connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(AppView)
+  mapDispatchToProps
+)(AppView);
 
 export default App;
