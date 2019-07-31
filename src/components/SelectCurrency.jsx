68d143c1021c70dcff4655ece45f65a3f2de53e8
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-07-31 16:50:53 -0500

    (component) update SelectCurrency
    add Currecy Flag, Prettier,

--- src/components/SelectCurrency.jsx
+++ src/components/SelectCurrency.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Select } from 'antd';
+import React from "react";
+import { Select } from "antd";
+import CurrencyFlag from "react-currency-flags";
 const { Option } = Select;
 
-const SelectCurrency = (props) => {
-    const { handleChange, currenciesList, baseCurrency } = props;
-    return(
-        <Select value={baseCurrency} style={{ width: 120 }} onChange={handleChange}>
-            {
-                currenciesList.map( currency => <Option key={currency} value={currency}>{currency}</Option>)
-            }
-        </Select>
-    );
-  }
+const SelectCurrency = props => {
+  const { handleChange, currenciesList, baseCurrency } = props;
+  return (
+    <Select value={baseCurrency} onChange={handleChange}>
+      {currenciesList.map(currency => (
+        <Option key={currency} value={currency}>
+          <CurrencyFlag currency={currency} size="sm" /> {currency}
+        </Option>
+      ))}
+    </Select>
+  );
+};
 
-export { SelectCurrency }
+export { SelectCurrency };
